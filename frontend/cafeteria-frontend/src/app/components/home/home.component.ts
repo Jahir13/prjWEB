82b8f3dc600394a7f5e2d1510abd66ac000ff210
Jahir13 <jahir.rocha@epn.edu.ec>
2025-01-29 02:25:32 -0500

    Backend terminado para los servicios actuales Frontend Home creado

--- frontend/cafeteria-frontend/src/app/components/home/home.component.ts
+++ frontend/cafeteria-frontend/src/app/components/home/home.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  images: string[] = [
+    'assets/carrusel.webp',
+    'assets/carrusel1.webp',
+    'assets/carrusel2.webp',
+    'assets/carrusel4.webp',
+    'assets/carrusel5.webp',
+    'assets/carrusel3.webp',
+    'assets/carrusel6.webp',
+    'assets/carrusel7.webp',
+    'assets/carrusel8.webp'
+  ];
 
+  currentIndex = 0;
+
+  constructor() {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 3000); // Cambia la imagen cada 3 segundos
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
